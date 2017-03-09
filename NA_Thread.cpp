d4fecec3ee8fef65c0d405cd7f61aa6730e0e03f
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-03-09 16:36:47 +0000

    now multithreading should be good to go

--- NA_Thread.cpp
+++ NA_Thread.cpp
@@ -1,6 +1,12 @@
-#include "stdafx.h"
 #include "NA_Thread.h"
+#include "globals.h"
+#include <iostream>
+using std::cerr;
 
+void NA_Thread::startThread()
+{
+	threadHandle = CreateThread(NULL, 0, threadProc, this, 0, threadID);
+}
 
 NA_Thread::NA_Thread()
 {
@@ -9,4 +15,41 @@ NA_Thread::NA_Thread()
 
 NA_Thread::~NA_Thread()
 {
+	requestSelfTerminate();
+	if (threadHandle)
+	{
+		LPDWORD exitCode = getExitCode();
+		if ((DWORD)exitCode == STILL_ACTIVE)
+			WaitForSingleObject(threadHandle, INFINITE);
+		CloseHandle(threadHandle);
+	}
+	
 }
+
+
+void NA_Thread::requestSelfTerminate()
+{
+	terminateRequested = true;
+}
+
+LPDWORD NA_Thread::getExitCode()
+{
+	LPDWORD exitCode;
+	if (GetExitCodeThread(threadHandle, exitCode) != 0);
+	{
+		consoleOutputCritSect.enter();
+		cerr << "NA_Thread::Destructor - failed to get thread exit code. Error: " << GetLastError() << "\n";
+		consoleOutputCritSect.leave();
+	}
+	return exitCode;
+}
+
+bool NA_Thread::waitForTerminate(DWORD milliseconds)
+{
+	DWORD w = WaitForSingleObject(threadHandle, milliseconds);
+	
+	return !(w == WAIT_TIMEOUT); //LOW: Should check for errors https://msdn.microsoft.com/en-us/library/windows/desktop/ms687032(v=vs.85).aspx
+}
+
+
+
--- NA_Thread.h
+++ NA_Thread.h
@@ -1,8 +1,40 @@
 #pragma once
-class NA_Thread
+
+#include <windows.h>
+#include "NA_CriticalSection.h"
+
+// http://www.flipcode.com/archives/Simple_Win32_Thread_Class.shtml
+// http://kaisar-haque.blogspot.co.uk/2007/07/c-nice-thread-class_23.html <- mainly used this one
+
+
+class NA_Thread //To make a thread, inherit this class and overload threadFunc, information will need to be global
+	//threads should check if terminateRequested is true and cleanup and return
+	//code will not exit properly if thread doesn't eventually terminate
 {
+private:
+	bool ready = false;
+	bool terminateRequested = false;
+	
+	HANDLE threadHandle = NULL;
+	LPDWORD threadID;
+	static DWORD WINAPI threadProc(LPVOID lpParameter)
+	{
+		NA_Thread* t = reinterpret_cast<NA_Thread*> (lpParameter);
+		return t->threadFunc();
+		
+	}
+
+protected:
+	virtual DWORD threadFunc() = 0;
+	void* dataObj = NULL;
+
 public:
+	void startThread();
 	NA_Thread();
-	~NA_Thread();
+	virtual ~NA_Thread();
+	void requestSelfTerminate();
+	LPDWORD getExitCode();
+	bool waitForTerminate(DWORD milliseconds = INFINITE);
 };
+
 
