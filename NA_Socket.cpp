ea1489cc6fe9bf81795a2e27c009e4dc5557a524
Nephi (AKA Lupus590) <lupus590@users.noreply.github.com>
2017-04-05 22:43:54 +0100

    acctual progres, need to speed up though

--- NA_Socket.cpp
+++ NA_Socket.cpp
@@ -1,4 +1,7 @@
 #include "NA_Socket.h"
+#include "globals.h"
+#include <iostream>
+using std::cerr;
 
 
 NA_Socket::NA_Socket()
@@ -10,9 +13,98 @@ NA_Socket::~NA_Socket()
 {
 }
 
-bool NA_Socket::init(MODE mode)
+bool NA_Socket::init(SOCK_TYPE sockType, SOCK_MODE sockMode)
 {
+	
+	switch (sockType)
+	{
+		case SOCK_TYPE::NOT_SET:
+			return false;
+			break;
+		case SOCK_TYPE::TCP:
+			return initTCP(sockMode);
+			break;
+		case SOCK_TYPE::UDP:
+			return initUDP(sockMode);
+			break;
+		default:
+			NA_consoleOutputCritSect.enter();
+			cerr << "NA_Socket::init - unhandled case\n";
+			NA_consoleOutputCritSect.leave();
+			return false;
+			break;
+	}
+	
 
+	return false;
+}
 
+bool NA_Socket::initUDP(SOCK_MODE sockMode)
+{
+	switch (sockMode)
+	{
+		case SOCK_MODE::NOT_SET:
+			return false;
+			break;
+		case SOCK_MODE::CLIENT:
+			return initUDPClient();
+			break;
+		case SOCK_MODE::SERVER:
+			return initUDPServer();
+			break;
+		default:
+			NA_consoleOutputCritSect.enter();
+			cerr << "NA_Socket::initUDP - unhandled case\n";
+			NA_consoleOutputCritSect.leave();
+			return false;
+			break;
+	}
+
+	return false;
+}
+
+bool NA_Socket::initTCP(SOCK_MODE sockMode)
+{
+	switch (sockMode)
+	{
+		case SOCK_MODE::NOT_SET:
+			return false;
+			break;
+		case SOCK_MODE::CLIENT:
+			return initTCPClient();
+			break;
+		case SOCK_MODE::SERVER:
+			return initTCPServer();
+			break;
+		default:
+			NA_consoleOutputCritSect.enter();
+			cerr << "NA_Socket::initTCP - unhandled case\n";
+			NA_consoleOutputCritSect.leave();
+			return false;
+			break;
+	}
+
+	return false;
+
+	return false;
+}
+
+bool NA_Socket::initUDPClient()
+{
+	return false;
+}
+
+bool NA_Socket::initUDPServer()
+{
+	return false;
+}
+
+bool NA_Socket::initTCPClient()
+{
+	return false;
+}
+
+bool NA_Socket::initTCPServer()
+{
 	return false;
 }
--- NA_Socket.h
+++ NA_Socket.h
@@ -3,13 +3,21 @@
 #include "NA_Message.h"
 class NA_Socket
 {
-private:
-	SOCKET sock;
 
 public:
-	static enum MODE {NOT_SET, UDP, TCP};
+	static enum SOCK_TYPE {NOT_SET, UDP, TCP};
+	static enum SOCK_MODE {NOT_SET, SERVER, CLIENT};
 	NA_Socket();
 	~NA_Socket();
-	bool init(MODE mode);
+	bool init(SOCK_TYPE sockType, SOCK_MODE sockMode);
+
+private:
+	SOCKET sock;
+	bool initUDP(SOCK_MODE sockMode);
+	bool initTCP(SOCK_MODE sockMode);
+	bool initUDPClient();
+	bool initUDPServer();
+	bool initTCPClient();
+	bool initTCPServer();
 };
 
